2babb2512f52befd1b1d83ac806fe3070e4c6c1f
Dean Hamann <hamann.dean@gmail.com>
2018-03-01 23:23:11 -0800

    Create Order and Product models

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/sampledata');
+
+var db = mongoose.connection;
+
+db.once('open', function(){
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
+
+db.on('error', function(err){
+  console.error(`Database error:\n${err}`);
+});
