6b267c26ad701de10d3d995c0001774282a62f61
Dean Hamann <hamann.dean@gmail.com>
2018-03-25 21:54:40 -0700

    Edit CartList component

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 const CartPage = (props) => {
   return (
     <div>
-      {props.order}
+         <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.order.products.map(product =>
+            <tr>
+              <td>{product._id}</td>
+              <td>{product.quantity}</td>
+            </tr>)}
+        </tbody>
+      </table>
+    </div>
     </div>
   )
 }
